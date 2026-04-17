89cf172119dbb8ea2cb1687cf06980d89002be39
benjamincjackson <benjamincjackson@gmail.com>
2026-04-17 19:00:52 +0100

    Formatting

--- src/regenie_add_pval_col/main.rs
+++ src/regenie_add_pval_col/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
-use std::io;
 use std::error::Error;
+use std::io;
 use std::process;
 
 use gwas_utilities::{open_reader, open_writer};
@@ -8,11 +8,7 @@ use gwas_utilities::{open_reader, open_writer};
 const USAGE: &str = "regenie_add_pval_col -i infile.regenie[.gz] -o outfile.regenie[.gz]";
 
 #[derive(Parser, Debug)]
-#[command(version, 
-    about, 
-    long_about = "Adds a P column to a Regenie output file based on the LOG10P column. The P column is added as the last column in the file. If the LOG10P value is large enough that the corresponding P value would be smaller than the smallest positive normal number representable in f64, then the P value is set to that smallest positive normal number (f64::MIN_POSITIVE) to avoid underflow issues when converting back and forth between log10(P) and P.",
-    override_usage = USAGE
-)]
+#[command(version, override_usage = USAGE, about = "Adds a P column to a Regenie output file based on the LOG10P column. The P column is added as the last column in the file. If the LOG10P value is large enough that the corresponding P value would be smaller than the smallest positive normal number representable in f64, then the P value is set to that smallest positive normal number (f64::MIN_POSITIVE) to avoid underflow issues when converting back and forth between log10(P) and P.")]
 struct Args {
     /// Regenie output file to process (can be gzipped if filename ends with .gz)
     #[arg(short, long)]
@@ -32,27 +28,27 @@ fn main() {
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
-    let (file_rdr, file_wtr) = handle_command_line_args()?;
+    let (file_rdr, file_wtr) = handle_commandline_args()?;
     process_file(file_rdr, file_wtr)
 }
 
-fn handle_command_line_args() -> Result<(gwas_utilities::Reader, gwas_utilities::Writer), Box<dyn Error>> {
+fn handle_commandline_args()
+-> Result<(gwas_utilities::Reader, gwas_utilities::Writer), Box<dyn Error>> {
     let args = Args::parse();
     let file_rdr = open_reader(&args.input)?;
     let file_wtr = open_writer(&args.output)?;
     Ok((file_rdr, file_wtr))
 }
 
-fn process_file<R, W>(fr: R, fw: W) -> Result<(), Box<dyn Error>> 
-    where 
-        R: io::Read,
-        W: io::Write
-    {
-
+fn process_file<R, W>(rdr: R, wtr: W) -> Result<(), Box<dyn Error>>
+where
+    R: io::Read,
+    W: io::Write,
+{
     let mut csv_rdr = csv::ReaderBuilder::new()
         .has_headers(true)
         .delimiter(b' ')
-        .from_reader(fr);
+        .from_reader(rdr);
 
     let mut header = csv_rdr.headers()?.clone();
     let (mut log10_p_col_idx, mut log10_p_col_found) = (0, false);
@@ -64,14 +60,12 @@ fn process_file<R, W>(fr: R, fw: W) -> Result<(), Box<dyn Error>>
         }
     }
     if !log10_p_col_found {
-        return Err("couldn't find LOG10P column in file header".into())
+        return Err("couldn't find LOG10P column in file header".into());
     }
 
     header.push_field("P");
 
-    let mut csv_wtr = csv::WriterBuilder::new()
-        .delimiter(b' ')
-        .from_writer(fw);
+    let mut csv_wtr = csv::WriterBuilder::new().delimiter(b' ').from_writer(wtr);
 
     csv_wtr.write_record(&header)?;
 
@@ -83,7 +77,7 @@ fn process_file<R, W>(fr: R, fw: W) -> Result<(), Box<dyn Error>>
         record.push_field(&ps);
         csv_wtr.write_record(&record)?;
     }
-    
+
     csv_wtr.flush()?;
 
     Ok(())
@@ -100,6 +94,7 @@ fn get_p_from_log10_p(log10_p: f64) -> f64 {
 #[cfg(test)]
 mod tests {
     use std::fs;
+    use std::io::Cursor;
 
     use super::*;
 
@@ -124,7 +119,7 @@ mod tests {
     #[test]
     fn add_p_to_file() {
         let file_rdr = open_reader("testdata/small.concat.regenie").unwrap();
-        let mut wtr = std::io::Cursor::new(Vec::new());
+        let mut wtr = Cursor::new(Vec::new());
         process_file(file_rdr, &mut wtr).unwrap();
         let desired_result = fs::read_to_string("testdata/small.concat.P.regenie").unwrap();
         let result = String::from_utf8(wtr.into_inner()).unwrap();
